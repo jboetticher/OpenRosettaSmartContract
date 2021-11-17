f10b38e0fd9dafa46f4c8cfc3aeb2fcf5ff5f15f
Simon Ware <simon.ware@openrosetta.org>
2021-11-17 15:28:31 +1300

    fixed arlocal

--- startlocal.js
+++ startlocal.js
@@ -1,11 +1,24 @@
-const { exec } = require("child_process");
+const ArLocal = require('arlocal');
+const Arweave = require("arweave");
 
-async function main() {
-    console.log('arlocal')
-    await exec('npx arlocal 8080')
-    console.log('start')
-    await exec('node startmining')
-    console.log('end')
-}
+const port = 8080;
 
-main()
+(async () => {
+  const arLocal = new ArLocal.default(port, false);
+
+  // Start is a Promise, we need to start it inside an async function.
+  await arLocal.start();
+
+  // Your tests here...
+
+  const arweave = Arweave.init({
+    host: "localhost",
+    port: port,
+    protocol: "http",
+  });
+
+  const continually_mine = setInterval(async function () {
+    await arweave.api.get("mine");
+    console.log("mined");
+  }, 6000);
+})();
